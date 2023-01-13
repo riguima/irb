f2c6a9fc2cdb6e3f20c36302fbd38493a161594f
ri-guima <richard.alexsander.guima@gmail.com>
2023-01-13 15:38:38 +0000

    tests: add tests for generate df

--- tests/test_comments.py
+++ tests/test_comments.py
@@ -1,6 +1,17 @@
+import pytest
 import json
+import pandas
 
 
-def test_get_comments_from_post():
-    expected = json.load('tests/expected_comments.json')
-    assert get_comments_from_post('https://www.instagram.com/p/CnCS-Y8rM_q/')[:20] == expected
+@pytest.fixture
+def comments():
+    return get_comments_from_post('https://www.instagram.com/p/CnCS-Y8rM_q/')
+
+
+def test_comments(comments):
+    assert comments[:20] == json.load('tests/expected_comments.json')
+
+
+def test_generate_df(comments):
+    expected_df = pandas.read_excel('tests/expected_worksheet.ods')
+    assert generate_df(comments) == expected_df
