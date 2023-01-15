5c20a6402194387b0a7e45ad0fa2d8aecc4beca0
ri-guima <richard.alexsander.guima@gmail.com>
2023-01-15 11:11:54 -0300

    tests: add tests for make login

--- irb/main.py
+++ irb/main.py
@@ -1,16 +1,26 @@
-import requests
-import pandas
-import os
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def get_comments_from_post(post):
     load_dotenv('.env')
-    session = requests.Session()
-    session.auth = ('marcela_morilo@hotmail.com', '23102019rml')
-    with session:
-        response = session.get(post)
-        soup = BeautifulSoup(str(response.content), 'lxml')
-        result = [comment.get_text() for comment in soup.select('.x1i10hfl')]
+    browser = create_browser()
+    make_login(browser)
+    browser.get(post)
+    print('username' in browser.page_source)
+    soup = BeautifulSoup(str(browser.page_source), 'lxml')
+    result = [comment.get_text() for comment in soup.select('.x1i10hfl')]
     return result
+
+
+def create_browser():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    return webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
+
+def make_login(browser):
+    browser.get('https://www.instagram.com/')
--- tests/test_comments.py
+++ tests/test_comments.py
@@ -1,12 +1,22 @@
 import pytest
 import json
 import pandas as pd
-from irb.main import get_comments_from_post
+from irb.main import get_comments_from_post, make_login, create_browser
 
 
 @pytest.fixture
 def comments():
     return get_comments_from_post('https://www.instagram.com/p/CnCS-Y8rM_q/')
+
+
+@pytest.fixture
+def browser():
+    return create_browser()
+
+
+def test_make_login(browser):
+    make_login(browser)
+    assert 'Messages' in browser.page_source
 
 
 def test_comments(comments):
