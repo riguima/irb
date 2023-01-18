2c19610a2ac13349099ce3fc2cfbaf10db7d7ea3
ri-guima <richard.alexsander.guima@gmail.com>
2023-01-18 16:48:09 -0300

    feat: add folder browser input

--- irb/app.py
+++ irb/app.py
@@ -5,12 +5,14 @@ from main import get_comments_from_post, generate_df
 
 if __name__ == '__main__':
     layout = [
-        [sg.Text('Código do post'), sg.InputText()],
+        [sg.Text('Código do post'), sg.InputText(key='post_code')],
+        [sg.Text('Pasta para download'), sg.Input(key='folder'),
+         sg.FolderBrowse(target='folder')],
         [sg.Button('Salvar Planilha')],
     ]
-    window = sg.Window('Bot Instagram', layout)
+    window = sg.Window('Bot Instagram', layout, resizable=False)
     while True:
         event, values = window.read()
         if event == 'Salvar Planilha':
-            df = generate_df(get_comments_from_post(values[0]))
-            df.to_excel()
+            df = generate_df(get_comments_from_post(values['post_code']))
+            df.to_excel(values['folder'])
