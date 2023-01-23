87d2b78bb9add419bb0ac6cbe62158d19d96e406
ri-guima <richard.alexsander.guima@gmail.com>
2023-01-23 00:05:22 -0300

    feat: modify gui

--- irb/app.py
+++ irb/app.py
@@ -1,6 +1,6 @@
 import PySimpleGUI as sg
 
-from main import get_comments_from_post, generate_df
+from main import get_votes_from_post, generate_df
 
 
 if __name__ == '__main__':
@@ -8,12 +8,12 @@ if __name__ == '__main__':
     layout = [
         [sg.Text('Código do post'), sg.InputText(key='post_code')],
         [sg.Text('Pasta para download'), sg.Input(key='folder'),
-         sg.FolderBrowse(target='folder')],
+         sg.FolderBrowse('Selecionar', target='folder')],
         [sg.Button('Salvar Planilha')],
     ]
     window = sg.Window('Bot Instagram', layout, resizable=False)
     while True:
         event, values = window.read()
         if event == 'Salvar Planilha':
-            df = generate_df(get_comments_from_post(values['post_code']))
+            df = generate_df(get_votes_from_post(values['post_code']))
             df.to_excel(values['folder'])
